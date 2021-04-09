63dc9f83e71590be831a86c40a75222fa053caef
shubham567 <shubhamshekhar567@gmail.com>
2021-04-10 02:32:03 +0530

    Text Improvement Home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 
 const HomePage = props => {
 
   return (
     <React.Fragment>
-      <Grid container>
+      <Grid container justify="center">
         <Grid item>
-          Hello
+          <Typography variant="h1">Welcome To Your Homepage</Typography>
         </Grid>
       </Grid>
     </React.Fragment>
